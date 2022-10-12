5622e1ec41f6a986729bab7adb5c4b03639210bc
Arsen <xakars02@gmail.com>
2022-10-12 15:58:56 +0600

    fix naming

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def validate(year: int):
+def get_ending_for_year(year: int):
     rule = {
         "1": "год",
         "2": "года",
@@ -25,38 +25,50 @@ def validate(year: int):
         return rule.get("3")
 
 
-def get_wines_from_exel():
-    load_dotenv()
-    path = os.environ["PATH_TO_EXEL_FILE"]
+def get_wines_from_exel(path: str) -> dict:
     excel_data_df = pandas.read_excel(
         path,
         names=['category','title','variety','price','image', 'promo'],
         keep_default_na=False)
-    table = excel_data_df.to_dict(orient='records')
+    wines_from_exel = excel_data_df.to_dict(orient='records')
     wines = defaultdict(list)
-    for line in table:
-        wines[line['category']].append(line)
+    for wine in wines_from_exel:
+        wines[wine['category']].append(wine)
     return wines
 
 
-def get_together_year():
+def get_winery_age() -> int:
     founded_year = 1920
     current_year = datetime.datetime.now().year
     return current_year-founded_year
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-template = env.get_template('template.html')
-rendered_page = template.render(
-    year=get_together_year(),
-    ending=validate(get_together_year()),
-    wines=get_wines_from_exel())
+def start_server(year: int, ending: str, wines: dict) -> None:
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        year=year,
+        ending=ending,
+        wines=wines)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+def main():
+    load_dotenv()
+    path = os.environ["PATH_TO_EXEL_FILE"]
+    wines = get_wines_from_exel(path)
+    age = get_winery_age()
+    ending = get_ending_for_year(age)
+    start_server(age, ending, wines)
+
+
+if __name__ == '__main__':
+    main()
